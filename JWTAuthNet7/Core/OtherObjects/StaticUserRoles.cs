68cc7a87b1d340198c0fc500bb8296ebebb448d3
Taiwo Folarin <tmfolarin@sidmach.com>
2024-03-06 16:07:42 +0100

    Added Dto and static roles

---
+++ JWTAuthNet7/Core/OtherObjects/StaticUserRoles.cs
@@ -0,0 +1,9 @@
+﻿namespace JWTAuthNet7.Core.OtherObjects
+{
+    public class StaticUserRoles
+    {
+        public const string OWNER = "OWNER";
+        public const string ADMIN = "ADMIN";
+        public const string USER = "USER";
+    }
+}
